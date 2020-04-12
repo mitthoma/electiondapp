399dfaad048c70b1b61738cb68d8be3a55c6d04f
Mitchell Thomas <mitchellthomas@mitchells-mbp.attlocal.net>
2020-04-12 00:51:47 -0400

    finally a working commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,16 +2,38 @@ pragma solidity ^0.5.16;
 
 contract Election {
 
-	// store candidate
-	// read candidate
+	//model a candidate
 
-	// declared state variable
-	string public candidate;
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
 
-	// constructor
+	// store candidates
+	// store with mapping
+	// fetch candidates
 
-	function my_Election() public {
-		candidate = "Candidate 1";
+
+	mapping(uint => Candidate) public candidates;
+
+	// store candidate's count
+	// use counter cache to keep track of how many candidates there are
+	// state variable
+	uint public candidatesCount;
+	
+
+	constructor () public {
+
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+		
+	}
+
+	function addCandidate( string memory _name) private {
+
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 
 	}
 
